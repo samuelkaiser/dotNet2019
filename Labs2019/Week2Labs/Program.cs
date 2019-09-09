52cc906a767811d8d59ebaf024d1e569da2faadf
Sam Kaiser <samuelkaisergithub@gmail.com>
2019-09-09 18:53:28 -0500

    adding all the initial lab files

---
+++ Labs2019/Week2Labs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Week2Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            DateTime today = new DateTime();
+            Console.WriteLine(today);
+            Console.WriteLine(DateTime.Now.ToString("MMMM"));
+            Console.ReadLine();
+            string year = DateTime.Now.ToString("YYYY");
+            string month = DateTime.Now.ToString("MMMM");
+        }
+    }
+}
